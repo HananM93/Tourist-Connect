981d3ef4b41cb2dd377b4c48cc4a8ef038596912
Hanan mohamed <hananmohamed@Hanans-MacBook-Air.local>
2023-03-12 16:28:39 -0500

    switched to postgres

--- db/migrate/20230312035742_create_guides.rb
+++ db/migrate/20230312035742_create_guides.rb
@@ -1,8 +1,8 @@
 class CreateGuides < ActiveRecord::Migration[7.0]
   def change
     create_table :guides do |t|
-      t.string :name
-      t.string :location
+      t.text :name
+      t.text :location
       t.integer :rate
 
       t.timestamps
